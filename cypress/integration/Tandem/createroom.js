6bf1c27cc8550e74039ca4e612b99fb55cf9fcbd
Patrick Dancel <47231845+patrickdancel@users.noreply.github.com>
2021-10-04 17:23:58 -0700

    Add files via upload

---
+++ cypress/integration/Tandem/createroom.js
@@ -0,0 +1,33 @@
+describe('Create room', () => {
+    it('creates room, join room, delete room', () => {
+        cy.visit('https://staging.tandem.chat/app/web')
+        cy.get('[data-testid=google]').click()
+        cy.wait(7000)
+        //click on add room
+        cy.get(':nth-child(5) > :nth-child(3) > .sc-csTaMs > .sc-bdnylx').click()
+        cy.wait(2000) 
+        cy.get('.sc-kHWWFa > :nth-child(1)').click()
+
+        // type name of room
+        cy.get('#room_name').click().type('QA Room Test')
+        cy.wait(1000)
+
+        // click on add members then done
+        cy.get('.sc-cBoprd').click()
+        cy.wait(1000)
+        cy.get('.sc-cBoprd').click()
+        cy.get('.kIdrfC > :nth-child(2) > .sc-bdnylx > .sc-hiKfjK').click()
+
+        // join room
+        cy.get(':nth-child(11) > .sc-gtssRu > .eWwHwG > .sc-dlnjPT').click()
+        cy.get('.bqAyor').click()
+        cy.wait(20000)
+        cy.get('#EndCall').click()
+
+        // delete room
+        cy.get(':nth-child(11) > .sc-gtssRu > .eWwHwG > .sc-dlnjPT > .sc-hHEjAm').rightclick()
+        cy.get('.gxYZqA > .sc-bdnylx').click()
+        cy.wait(1000)
+        cy.get('.gLflUe').click()
+    })
+})

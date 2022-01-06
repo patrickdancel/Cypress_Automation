02b89702ad620ce046951a38a2bdf8fbe0092493
Patrick Dancel <47231845+patrickdancel@users.noreply.github.com>
2022-01-05 17:32:36 -0800

    Add files via upload

--- cypress/integration/Tandem/settings.js
+++ cypress/integration/Tandem/settings.js
@@ -5,23 +5,23 @@ describe('Join table', () => {
         cy.wait(7000)
 
         cy.wait(3000)
-        cy.get(':nth-child(4) > .SimpleTooltip__TriggerWrapper-sc-9fovao-0 > .bebbWG > .src__Root-xohg5z-0').click()
+        cy.get(':nth-child(6) > .SimpleTooltip__TriggerWrapper-sc-9fovao-0 > .bebbWG > .src__Root-xohg5z-0').click()
         cy.wait(2000)
         // toggle dark mode
         cy.get('.PopoverMenu__Wrapper-btsdzh-0 > :nth-child(1) > .src__Row-xohg5z-2').click()
         cy.wait(2000)
 
         // click on chrome extension
-        cy.get(':nth-child(4) > .SimpleTooltip__TriggerWrapper-sc-9fovao-0 > .bebbWG > .src__Root-xohg5z-0').click()
+        cy.get(':nth-child(6) > .SimpleTooltip__TriggerWrapper-sc-9fovao-0 > .bebbWG > .src__Root-xohg5z-0').click()
         cy.wait(3000)
         cy.get('.PopoverMenu__Wrapper-btsdzh-0 > :nth-child(4) > .src__Root-xohg5z-0').click()
 
         // click on desktop  app
-        cy.get(':nth-child(4) > .SimpleTooltip__TriggerWrapper-sc-9fovao-0 > .bebbWG > .src__Root-xohg5z-0').click()
+        cy.get(':nth-child(6) > .SimpleTooltip__TriggerWrapper-sc-9fovao-0 > .bebbWG > .src__Root-xohg5z-0').click()
         cy.wait(3000)
         cy.get('.PopoverMenu__Wrapper-btsdzh-0 > :nth-child(5) > .src__Root-xohg5z-0').click({force: true, multiple: true})
         // click on mobile app
-        cy.get(':nth-child(4) > .SimpleTooltip__TriggerWrapper-sc-9fovao-0 > .bebbWG > .src__Root-xohg5z-0').click()
+        cy.get(':nth-child(6) > .SimpleTooltip__TriggerWrapper-sc-9fovao-0 > .bebbWG > .src__Root-xohg5z-0').click()
         cy.wait(3000)
         cy.get('.PopoverMenu__Wrapper-btsdzh-0 > :nth-child(6) > .src__Root-xohg5z-0').click({force: true, multiple: true})
       

b9b0ce135b72469d6858d52ad37bfa3ab3229c01
Patrick Dancel <47231845+patrickdancel@users.noreply.github.com>
2022-01-05 17:32:22 -0800

    Add files via upload

--- cypress/integration/Tandem/switchingTeams.js
+++ cypress/integration/Tandem/switchingTeams.js
@@ -5,15 +5,15 @@ describe('Switching teams', () => {
         cy.wait(7000)
         // click on team
         cy.wait(3000)
-        cy.get('.text__T15-sc-1vvycwd-8').click()
+        cy.get('.gGtCgX > :nth-child(1) > .bebbWG > .PressablePrimitive__RootPressable-sc-130th01-0 > .Icon__StyledMaterialIcon-idxmxm-1').click()
         cy.wait(2000)
         // switches team
         cy.get(':nth-child(2) > .obfkP > .Alphatar__Wrapper-sc-1b47z1e-0 > .src__Root-xohg5z-0').click()
         cy.wait(2000)
         // click on team dropdown switch back team
-        cy.get('.text__T15-sc-1vvycwd-8').click()
+        cy.get('.gGtCgX > :nth-child(1) > .bebbWG > .PressablePrimitive__RootPressable-sc-130th01-0 > .Icon__StyledMaterialIcon-idxmxm-1').click()
         cy.wait(1000)
-        cy.get(':nth-child(1) > .beJpkO > .sc-amjdv').click()
+        cy.get(':nth-child(1) > .obfkP > .src__Column-xohg5z-1 > .text__T16-sc-1vvycwd-7').click()
 
     })
 })

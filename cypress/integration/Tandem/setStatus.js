a81e63237fc36a7ee3b579bb36ef541ceb851b40
Patrick Dancel <47231845+patrickdancel@users.noreply.github.com>
2022-01-05 17:32:46 -0800

    Add files via upload

--- cypress/integration/Tandem/setStatus.js
+++ cypress/integration/Tandem/setStatus.js
@@ -5,30 +5,30 @@ describe('Status', () => {
         cy.get('[data-testid=google]').click()
         cy.wait(7000)
         // click on status
-        cy.get('.main__RadialSkrim-sc-1545u64-9').click()
+        cy.get('.PressablePrimitive__RootPressable-sc-130th01-0 > .ProfileImage__Wrapper-sc-1xu350m-0 > .main__RadialSkrim-sc-1545u64-9').click({multiple: true})
         
         cy.wait(2000)
        
         // type in status and save
-        cy.get('.Input__StyledInputBox-sc-1neqxsk-0 > .Input__StyledInputField-sc-1neqxsk-1').click().type('testing')
+        cy.get('.ModalPadding-gqxexn-0 > .Input__StyledInputBox-sc-1neqxsk-1').click().type('testing')
         cy.get('.Button__StyledButton-uamha2-1').click()
         cy.wait(2000)
 
         // clear status and close
-        cy.get('.main__RadialSkrim-sc-1545u64-9').click()
+        cy.get('.PressablePrimitive__RootPressable-sc-130th01-0 > .ProfileImage__Wrapper-sc-1xu350m-0 > .main__RadialSkrim-sc-1545u64-9').click()
         cy.wait(3000)
         cy.get('.fOQYlV > .src__Root-xohg5z-0 > .Icon__StyledMaterialIcon-idxmxm-1').click()
         cy.get('#Close').click()
         cy.wait(2000)
 
         // select a status
-        cy.get('.main__RadialSkrim-sc-1545u64-9').click()
+        cy.get('.PressablePrimitive__RootPressable-sc-130th01-0 > .ProfileImage__Wrapper-sc-1xu350m-0 > .main__RadialSkrim-sc-1545u64-9').click()
         cy.wait(2000)
         cy.get(':nth-child(11) > .donBme').click()
         cy.wait(2000)
 
         // clear status
-        cy.get('.main__RadialSkrim-sc-1545u64-9').click()
+        cy.get('.PressablePrimitive__RootPressable-sc-130th01-0 > .ProfileImage__Wrapper-sc-1xu350m-0 > .main__RadialSkrim-sc-1545u64-9').click()
         cy.wait(3000)
         cy.get('.fOQYlV > .src__Root-xohg5z-0 > .Icon__StyledMaterialIcon-idxmxm-1').click()
         cy.get('#Close').click()
